24d9d512a58b390522ae152b7e4b817877cc3857
just <adm@biwt.ru>
2026-03-14 15:01:14 +0300

    Fix pubkey extraction in dev key script

--- scripts/gen_dev_keys.sh
+++ scripts/gen_dev_keys.sh
@@ -11,7 +11,7 @@ openssl genpkey -algorithm X25519 -out "${OUT_DIR}/server_static_priv.pem"
 openssl pkey -in "${OUT_DIR}/server_static_priv.pem" -pubout -out "${OUT_DIR}/server_static_pub.pem"
 
 SERVER_PRIV_B64="$(openssl pkey -in "${OUT_DIR}/server_static_priv.pem" -outform DER | tail -c 32 | base64)"
-SERVER_PUB_B64="$(openssl pkey -in "${OUT_DIR}/server_static_pub.pem" -outform DER | tail -c 32 | base64)"
+SERVER_PUB_B64="$(openssl pkey -pubin -in "${OUT_DIR}/server_static_pub.pem" -outform DER | tail -c 32 | base64)"
 
 echo "${SERVER_PRIV_B64}" > "${OUT_DIR}/server_static_priv.b64"
 echo "${SERVER_PUB_B64}" > "${OUT_DIR}/server_static_pub.b64"
